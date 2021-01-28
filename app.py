45acb5f920531acfed67142018a743b465701b3e
Riccardo M. Cefala <riccardo.cefala@container-solutions.com>
2021-01-28 11:51:22 +0100

    add route53 logic

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
+import os
+import sys
 
 from aws_cdk import core
 
 from route53_cdk_dyndns.route53_cdk_dyndns_stack import Route53CdkDyndnsStack
 
 
+def get_env_var_or_break(env_var):
+    try:
+        return os.environ[env_var]
+    except KeyError:
+        print("Environment variable %s must be set" % env_var)
+        sys.exit(1)
+
+
 app = core.App()
-Route53CdkDyndnsStack(app, "route53-cdk-dyndns")
+Route53CdkDyndnsStack(
+    app,
+    "route53-cdk-dyndns",
+    hosted_zone_id=get_env_var_or_break("R53_HOSTED_ZONE_ID"),
+    zone_name=get_env_var_or_break("R53_ZONE_NAME"),
+    record_name=get_env_var_or_break("R53_RECORD_NAME"),
+)
 
 app.synth()
--- route53_cdk_dyndns/route53_cdk_dyndns_stack.py
+++ route53_cdk_dyndns/route53_cdk_dyndns_stack.py
@@ -1,9 +1,33 @@
 from aws_cdk import core
+import aws_cdk.aws_route53 as route53
+from requests import get
 
 
 class Route53CdkDyndnsStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: core.Construct,
+        construct_id: str,
+        hosted_zone_id: str,
+        zone_name: str,
+        record_name: str,
+        **kwargs
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        ip = get("https://api.ipify.org").text
+
+        zone = route53.HostedZone.from_hosted_zone_attributes(
+            self,
+            "MyZone",
+            zone_name=zone_name,
+            hosted_zone_id=hosted_zone_id,
+        )
+
+        route53.ARecord(
+            self,
+            "ExternalIP",
+            record_name=record_name,
+            zone=zone,
+            target=route53.RecordTarget.from_ip_addresses(ip),
+        )
